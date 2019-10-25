d9cca05962fa6ae9f0e25d2d51e749fbfbecc475
Ubuntu <ubuntu@ip-172-31-84-101.ec2.internal>
2019-10-25 09:25:00 +0000

    settings.py databases & import env

--- fsf_ecommerce/settings.py
+++ fsf_ecommerce/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Import env.py to allow access to the environment variables
-# import env
+if os.path.exists('env.py'):
+    import env
 
 # Import dj_database_url which we installed with "pip3 install dj-database-url"
 import dj_database_url
@@ -96,16 +97,19 @@ WSGI_APPLICATION = 'fsf_ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
 
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+else:
+    print("Database URL not found. Using SQLite instead.")
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
