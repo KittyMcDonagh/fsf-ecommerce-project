1257c5a8fe3cb4ace77993a1dca4e7be225b3842
Ubuntu <ubuntu@ip-172-31-84-101.ec2.internal>
2019-10-25 10:14:41 +0000

    settings.py comments and tidyup

--- fsf_ecommerce/settings.py
+++ fsf_ecommerce/settings.py
@@ -23,7 +23,6 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -96,8 +95,6 @@ WSGI_APPLICATION = 'fsf_ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
